e83634cbeb3eacd581a9eb1d2e8fc420fdd30a43
franckgseka <franckgseka@gmail.com>
2022-11-24 13:56:53 +0100

    init

---
+++ src/page/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ea dolores odit ipsum officiis placeat quod iusto autem sunt nulla voluptatum, impedit, doloribus laborum.
+                Exercitationem perspiciatis unde eligendi nostrum aut magnam tenetur sit, harum consequatur quas explicabo nulla tempora suscipit labore delectus alias error quo, corrupti accusamus iure minima libero.
+                Modi corporis unde corrupti ducimus. Molestias nulla mollitia odio perferendis, repellat quo.
+                Nisi aperiam ipsam, quas laboriosam soluta nesciunt ab quis aliquid illum nostrum obcaecati. Doloremque vitae, similique officiis quod at nihil culpa expedita omnis asperiores quo ut, ea a, mollitia ad.
+                Inventore nostrum culpa fugiat, temporibus suscipit eum corrupti blanditiis.   
+            </p>
+
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Ea dolores odit ipsum officiis placeat quod iusto autem sunt nulla voluptatum, impedit, doloribus laborum.
+                Exercitationem perspiciatis unde eligendi nostrum aut magnam tenetur sit, harum consequatur quas explicabo nulla tempora suscipit labore delectus alias error quo, corrupti accusamus iure minima libero.
+                Modi corporis unde corrupti ducimus. Molestias nulla mollitia odio perferendis, repellat quo.
+                Nisi aperiam ipsam, quas laboriosam soluta nesciunt ab quis aliquid illum nostrum obcaecati. Doloremque vitae, similique officiis quod at nihil culpa expedita omnis asperiores quo ut, ea a, mollitia ad.
+                Inventore nostrum culpa fugiat, temporibus suscipit eum corrupti blanditiis.   
+            </p>
+        </div>
+    );
+};
+
+export default About;
